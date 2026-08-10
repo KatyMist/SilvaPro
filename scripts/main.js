7fd5b0e2f6abc4cf8161462c7c8afbc702572928
Ekaterina Tumanova <tumanova.ekaterina73@gmail.com>
2026-08-10 09:35:59 +0400

    Updates

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-import '../styles/style.scss';
-
-import { initNavHighlight } from './nav-highlight.js';
-import { initServiceCardsReveal } from './service-cards-reveal.js';
-
-initNavHighlight();
-initServiceCardsReveal();
