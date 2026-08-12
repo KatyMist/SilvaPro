727b6549fbc8b3f9872155a27228cf372bd3b973
Ekaterina Tumanova <tumanova.ekaterina73@gmail.com>
2026-08-12 15:57:44 +0400

    update

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/SilvaPro/',
+  base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+        notFound: resolve(__dirname, '404.html'),
+      },
+    },
+  },
 });
